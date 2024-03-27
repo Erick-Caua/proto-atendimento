e56257c922a2be12204cd64112eee110edc4d243
Erick-Caua <146395495+Erick-Caua@users.noreply.github.com>
2024-03-27 16:46:34 -0300

    at

--- scripts/scripts2.js
+++ scripts/scripts2.js
@@ -1,29 +1,61 @@
 const form = document.querySelector('.meuForm')
 const lista = document.querySelector('.lista')
-const liPaciente = lista.querySelectorAll('li')
+const emAtd = document.querySelector('.emAtendimento')
+const listaUrgencia = document.querySelector('.lista-urgencia')
+const listaPreferencial = document.querySelector('.lista-preferencial')
+
 
 
 function adicionarPaciente() {
     let paciente = form.pacienteC.value
     const data = new Date().toLocaleString()
-    if (form.urgencia.checked === true) {
-        paciente += ' - Urgência'
+    const novoPaciente = document.createElement('li')
 
+    if (form.urgencia.checked) {
+        paciente += ' - Urgência'
+        novoPaciente.textContent = `${paciente} - Horario de chegada: ${data}`
+        listaUrgencia.appendChild(novoPaciente)
+
+    } else if(form.preferencial.checked) {
+        paciente += ' - Preferencial'
+        novoPaciente.textContent = `${paciente} - Horario de chegada: ${data}`
+        listaPreferencial.appendChild(novoPaciente)
+    }else{
+        novoPaciente.textContent = `${paciente} - Horario de chegada: ${data}`
+        lista.appendChild(novoPaciente)
     }
 
-    let novoPaciente = document.createElement('li')
-    novoPaciente.textContent = `${paciente} - Horario de chegada: ${data}`
-    lista.appendChild(novoPaciente)
+
 
 
 }
 function limpar() {
     form.pacienteC.value = ``
     form.urgencia.checked = false
+    form.preferencial.checked = false
 }
 
 function atender() {
-  
+    const pacientes = listaUrgencia.querySelectorAll('li')
+    const pacientesN = lista.querySelectorAll('li')
+    const pacientesP = listaPreferencial.querySelectorAll('li')
+    const data = new Date().toLocaleString()
+
+    let pacienteEmAtendimento = ''
+    
+    if (pacientes.length > 0) {
+        pacienteEmAtendimento = `${pacientes[0].textContent} - Horario de atendimento: ${data}`
+        pacientes[0].remove()
+    } else if (pacientesP.length > 0) {
+        pacienteEmAtendimento = `${pacientesP[0].textContent} - Horario de atendimento: ${data}`;
+        pacientesP[0].remove();
+    } else if(pacientesN.length >0){
+        pacienteEmAtendimento = `${pacientesN[0].textContent} - Horario de atendimento: ${data}`;
+        pacientesN[0].remove();
+    }else{
+        pacienteEmAtendimento = `Sem pacientes em espera`
+    }
+    emAtd.textContent = pacienteEmAtendimento
 }
 
 
